85c2d633db3b28064711a8654a4ef49b4bbfd369
raffaelloferrari <47225245+raffaelloferrari@users.noreply.github.com>
2019-09-03 00:34:25 +0200

    Agent virtualization creation for Oracle VM #9

---
+++ fetch/ovm.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+#####################################################################################
+# Prereq   - ovm_vmcontrol installato su ovm manager
+#   	   - Scambio chiavi con utente ovm manager che possa eseguire ovm_vmcontrol
+#   	   - Le vm non devono contenere spazi
+#####################################################################################
+
+OVMPASS="Oracle123"
+OVMHOST=vmmgmt
+OVMUSER=admin
+OVMUSERKEY=root
+SSHOVMCLI="sshpass -p 'Oracle123' ssh admin@vmmgmt -p 10000"
+OVMCONTROL=/tmp/ovm/ovm_util/ovm-utils_2.1
+TYPE=$1
+CLUSTERCPU=0
+
+function check_connection
+{
+	ovmcli_access=$($SSHOVMCLI list manager |grep -c Success)
+	if [[ $ovmcli_access != 1 ]]; then
+	  exit 1
+	fi
+}
+
+function get_pool_from_vmname {
+	$SSHOVMCLI show vm name=$1 |grep "  Server Pool" |cut -d "[" -f2 |cut -d "]" -f1
+}
+
+function check_vmname_on_pool {
+	$SSHOVMCLI show ServerPool name=$1 |grep -c "\[$2\]"
+}
+
+function get_pool_from_server {
+	$SSHOVMCLI show server name=$1 |grep "  Server Pool = " |cut -d "[" -f2 |cut -d "]" -f1
+}
+
+function get_servers_from_pool {
+	$SSHOVMCLI show serverpool name=$1 |grep '  Server .* = .*:.*:.*:' |cut -d "[" -f2 |cut -d "]" -f1
+}
+
+function list_server_pool {
+	$SSHOVMCLI list serverpool |grep "  id:" |cut -d ":" -f3
+}
+
+function list_vm {
+	$SSHOVMCLI list vm |grep "  id:" |cut -d ":" -f3 | grep -v " "
+}
+
+function list_vm_with_space {
+	$SSHOVMCLI list vm |grep "  id:" |cut -d ":" -f3
+}
+
+function vm_with_space {
+	if [[ $(list_vm_with_space | grep " "|wc -l) -gt 0 ]]; then
+		echo "ERROR VM WITH SPACE"
+		list_vm_with_space | grep " "
+	fi
+}
+
+function get_cpu_pinned_vm {
+	ssh ${OVMUSERKEY}@${OVMHOST} "cd ${OVMCONTROL}; ./ovm_vmcontrol -u ${OVMUSER} -p ${OVMPASS} -h ${OVMHOST} -v $1 -c getvcpu"
+}
+
+function get_cpu_from_server {
+	$SSHOVMCLI show server name=$1 |grep "  Processors = " |awk '{print $4}'
+}
+
+
+if [ $# -ne 1 ]
+ then
+	clear
+	echo ""
+	echo "====================================================================================================="
+	echo " You have to specify <vms|cluster>"
+	echo " Example:"
+        echo "           ovm.sh vms"
+        echo "           ovm.sh cluster"
+	echo "====================================================================================================="
+	echo ""
+	exit -1
+fi
+
+if [[ $TYPE == "vms" ]]; then
+	for i in $(list_vm); do
+	if [[ $(get_cpu_pinned_vm $i|grep -c "Current pinned CPU") -eq 1 ]]; then
+		PINNED=1
+	else
+		PINNED=
+	fi
+	echo "$(get_pool_from_vmname "$i"),$i,,$PINNED"
+	done
+elif [[ $TYPE == "cluster" ]]; then
+	for i in $(list_server_pool); do
+		for j in $(get_servers_from_pool "$i"); do
+			CLUSTERCPU=$(($CLUSTERCPU + $(get_cpu_from_server $j)))
+		done
+		echo "$i,$CLUSTERCPU"
+	done
+
+else
+	echo "Option not valid"
+	exit -1
+fi
+
+###############################################
+# CAPIRE DOVE SEGNALARE ANOMALIA vm_with_space
+###############################################
+
+
