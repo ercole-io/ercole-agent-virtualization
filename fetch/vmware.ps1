8ba5acaa0bc236382fa30fb2e02f06fab28cc6f3
amreo <amreo@live.it>
2019-11-18 15:08:18 +0100

    Fixed guest hostname

--- fetch/vmware.ps1
+++ fetch/vmware.ps1
@@ -49,7 +49,7 @@ New-VIProperty -Name NumSockets -ObjectType Cluster -Value {
 switch ($s.ToUpper()) {
         "VMS" {
                 # OUTPUT FORMAT: cluster name, vm name, guest os hostname
-                Get-VM | Select @{N="Cluster";E={Get-Cluster -VM $_}}, Name,  @{N="guestHostname1";E={$_.VMHost}}, @{N="ESX Host";E={Get-VMHost -VM $_}} | ConvertTo-CSV | % { $_ -replace '"', ""}
+                Get-VM | Select @{N="Cluster";E={Get-Cluster -VM $_}}, Name, @{N="guestHostname";E={$_.ExtensionData.Guest.HostName}}, @{N="ESX Host";E={Get-VMHost -VM $_}} | ConvertTo-CSV | % { $_ -replace '"', ""}
         }
         "CLUSTER" {
                 # OUTPUT FORMAT: cluster name, core sum, socket sum
